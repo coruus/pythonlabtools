4669a0bfebf40364f6b5e0993b531484ccbfd9cf
Marcus Mendenhall <mendenhall@nist.gov>
2005-07-01 19:48:25 +0000

    simplified life by using all 31-bit integers, and made fast sequences identical to individual numbers

--- analysis/r250.py
+++ analysis/r250.py
@@ -59,34 +59,28 @@ Heuer, Dunweg and Ferrenberg,
 by Marcus Mendenhall
                                                                 
 """
-_rcsid="$Id: r250.py,v 1.8 2005-06-29 14:23:28 mendenhall Exp $"
+_rcsid="$Id: r250.py,v 1.9 2005-07-01 19:48:25 mendenhall Exp $"
 
 import random
 import Numeric
 import math
 
-#test for NumPy array conversion bias where UnsignedInt32 -> Float is actually signed
-a=Numeric.array((-1,),Numeric.UnsignedInt32).astype(Numeric.Float)[0]
-if a<0.0:
-	_numpy_conversion_bias=1
-else:
-	_numpy_conversion_bias=0
-
 class ran_shift(random.Random):
 	"generate shift-register based high-quality randoms"	
 	p,q = 250, 103
 	def __init__(self, seed=None):
-		self.floatscale=1.0/float(1L<<64)
-		self.single_floatscale=1.0/float(1L<<32)
+		self.floatscale=1.0/float(1L<<62)
+		self.single_floatscale=1.0/float(1L<<31)
 		self.seed(seed)
-		self.mask32=~Numeric.array(0xffff, Numeric.UnsignedInt32)
+		self.mask32=Numeric.array(0x7ffffe00, Numeric.Int32)
 		
 	def seed(self, seed=None):
 		"seed from the built in RNG python provides, and then scramble results a little"
 		random.Random.seed(self, seed)
 		seeds=[ 
-			int((long(math.floor(random.random()*65536.0)) << 16) | long(math.floor(random.random()*65536.0))) for i in range(self.p)]
-		self.ranbuf=Numeric.array(seeds, Numeric.UnsignedInt32)
+			int( ((long(math.floor(random.random()*32768.0)) << 16) & 0x7fff0000) | long(math.floor(random.random()*65536.0))) for i in range(self.p)]
+		self.ranbuf=Numeric.array(seeds, Numeric.Int32)
+		
 		for i in range(self.p): #scramble the dubious randoms from the default generator to start the system
 			self.regenerate()
 		
@@ -101,7 +95,7 @@ class ran_shift(random.Random):
 		self.counter=0
 
 	def next(self):
-		"return the next 32 bits from the random pool"
+		"return the next 31 bits from the random pool"
 		v=self.ranbuf[self.counter]
 		self.counter+=1
 		if self.counter==self.p:
@@ -112,12 +106,13 @@ class ran_shift(random.Random):
 		return Numeric.array(self.ranbuf), self.counter #make sure we copy the array, not just return a reference
 	
 	def setstate(self, state):
-		self.ranbuf, self.counter = state
+		self.ranbuf=Numeric.array(state[0]) #make a copy here, too, so we can re-use the state as often as needed
+		self.counter = state[1]
 		
 	def random(self):
 		"return a _really good_ double precision (52 bits significant) random on [0,1) (upper included bound = (1 - 2^-52) )"
 		q1, q2 = self.next(), self.next() #take 64 bits from the random pool
-		return float( (long(q1) << 32) | ( long(q2) & 0xfffff000L) ) *self.floatscale
+		return float( (long(q1) << 31) | ( long(q2) & 0x7ffffe00L) ) *self.floatscale
 
 	def single_float_random(self):
 		"return a good single-precision (32 bits significant) random on [0,1)  (upper included bound = (1 - 2^-32) )"
@@ -125,38 +120,43 @@ class ran_shift(random.Random):
 		return float(q1)*self.single_floatscale
 			
 	def fast_random_series(self, count):
-		"return a series of 32-bit ints. Most useful for count >> 250, since it wastes a lot of randoms, but is very fast"
-		results=Numeric.zeros(count, Numeric.UnsignedInt32)
-		for i in range(count/self.p):
+		"return a series of 31-bit ints much more quickly than individual calls to next()."
+		results=Numeric.zeros(count, Numeric.Int32)
+		first=min(count, self.p-self.counter) #how many are already in the buffer that we want?
+		results[:first]=self.ranbuf[self.counter:self.counter+first]
+		#print self.ranbuf[self.counter], results.tolist()
+		count-=first
+		self.counter+=first
+		if self.counter==self.p: #used up all in buffer, start generating more
 			self.regenerate()
-			results[self.p*i:self.p*(i+1)]=self.ranbuf
-		self.regenerate()
-		rest=count%self.p
-		if rest:
-			results[-rest:]=self.ranbuf[:rest]
-			self.counter=rest
+			for i in range(count//self.p):
+				results[first:first+self.p]=self.ranbuf
+				first+=self.p
+				count-=self.p
+				self.regenerate()
+			if count:
+				results[first:]=self.ranbuf[:count]
+				self.counter=count
 		return results
 
 	def single_float_random_series(self, count):
-		"return a series of good single-precision-quality (actually 32 bits embedded in double) randoms on [0,1).(upper included bound = (1 - 2^-32) )  \
+		"return a series of good single-precision-quality (actually 31 bits embedded in double) randoms on [0,1).(upper included bound = (1 - 2^-31) )  \
 				Note: conversion of this number to Float32 could result in rounding to exactly 1.0"
 
 		#Warning! NumPy Unsigned-> Float is really signed!
 		q1 = Numeric.array(self.fast_random_series(count), Numeric.Float) #take bits from the random pool. 
 		q1*=self.single_floatscale
-		q1+=_numpy_conversion_bias*0.5
 		return q1
 
 	def double_float_random_series(self, count):
 		"return a series of 52-bit significance double-precision randoms on [0,1)."
 		q1 = self.fast_random_series(2*count) #take bits from the random pool
-		q1[count:] &= self.mask32#mask off low bits to prevent rounding when combined to 52-bit mantissas
-		q1=q1*self.single_floatscale #convert results to doubles scaled on [0,1)
-		q1+=_numpy_conversion_bias*0.5
+		q1[1::2] &= self.mask32#mask off low bits to prevent rounding when combined to 52-bit mantissas
+		q1=q1*self.single_floatscale #convert results to doubles scaled on [0,1)		
 		#now, for any generator with two-point correlations, or unknown bit lengths, this would fail horribly, but this class is safe.
-		q1[count:]*=self.single_floatscale #rescale LSBs 
-		q1[:count]+=q1[count:] #and combine with MSBs
-		return q1[:count]
+		q1[1::2]*=self.single_floatscale #rescale LSBs 
+		q1[0::2]+=q1[1::2] #and combine with MSBs
+		return q1[::2]
 
 class r250(ran_shift):
 	"generate r250 based high-quality randoms"	
@@ -204,11 +204,58 @@ if __name__ == "__main__":
 	print
 
 	r.setstate(state)
-	
 	print r.single_float_random_series(10)
 	print
 
+	r.setstate(state)
+	print r.single_float_random_series(10)
+	print
+
+	import struct
+	
+	print "checking for uniformly filled bit in combined doubles"
+	r.setstate(state)
+	s=r.double_float_random_series(100).tostring()
+	ss=struct.unpack("20L", s[:80])
+	print (20*"%08lx ")%ss
+	b1, b2 =0, 0
+	for i,j in zip(ss[0::2], ss[1::2]):
+		b1, b2 = b1 | i, b2 | j
+	print "this should be 3fffffff ffffffff:", "%08lx %08lx" % (b1,b2)
+	
+	r.setstate(state)
+	ranlistfast=r.fast_random_series(1000).tolist()
+	r.setstate(state)
+	ranlist=[r.next() for i in range(1000)]
+	
+	if ranlist != ranlistfast:
+		print "fast series is not same as single randoms"
+		print zip(ranlist, ranlistfast)
+	else: print "fast series sequence is same as individual numbers"
+
+	r.setstate(state)
+	ranlistfast=r.single_float_random_series(1000).tolist()
+	r.setstate(state)
+	ranlist=[r.single_float_random() for i in range(1000)]
+	
+	if ranlist != ranlistfast:
+		print "fast single series is not same as single randoms"
+		print zip(ranlist, ranlistfast)
+	else: print "fast single series sequence is same as individual numbers"
+
+	r.setstate(state)
+	ranlistfast=r.double_float_random_series(1000).tolist()
+	r.setstate(state)
+	ranlist=[r.random() for i in range(1000)]
+	
+	if ranlist != ranlistfast:
+		print "fast double series is not same as single randoms"
+		print zip(ranlist, ranlistfast)
+	else: print "fast double series sequence is same as individual numbers"
+
+
 	if 1:
+		print "running pi-darts in 3d"
 		cycles=100
 		count=10000
 		sum=0.0
