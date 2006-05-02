4ad38f7332c695bc706a276e1e1647869231ef57
Marcus Mendenhall <mendenhall@nist.gov>
2006-05-02 20:44:10 +0000

    dDoxygen docs started

---
+++ analysis/analysis_mainpage.h
@@ -0,0 +1,20 @@
+/** \file
+\brief It looks like a header file, but it's really a giant comment with Doxygen documentation in it.
+*/
+/** \mainpage
+\author Marcus H. Mendenhall 
+\date 2006
+
+Copyright &copy; 2005 Vanderbilt University. All rights reserved.
+
+\section intro_sec Introduction to analysis
+
+\section overview_sec Overview
+
+
+\section use_case Typical Use Cases
+\subpage fitting_subsec 
+
+\page fitting_subsec Fitting functions
+The main documentation for this is in the fitting_toolkit package
+*/
