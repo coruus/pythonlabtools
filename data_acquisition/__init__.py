d9f8a5b9bb0cacc9a5c043ee3929ed906e8e8f23
David Leon Gil <coruus@gmail.com>
2014-10-17 00:34:39 -0400

    autopep8

--- data_acquisition/__init__.py
+++ data_acquisition/__init__.py
@@ -1,4 +1,2 @@
 "empty __init__ just to make a package"
 #$Id$
-
-
--- data_acquisition/hp_scope_module.py
+++ data_acquisition/hp_scope_module.py
@@ -1,18 +1,18 @@
 "module importing a scope for global access"
-_rcsid="$Id$"
+_rcsid = "$Id$"
 
 from vxi_11_scopes import hp54542
 
+
 class my_hp54542(hp54542):
-	pass
+  pass
 
 
 if 0:
-	scope=my_hp54542(host="", portmap_proxy_host="127.0.0.1", portmap_proxy_port=1111,
-			 device="gpib0,7",  timeout=4000, device_name="hp54542",
-			raise_on_err=1)
+  scope = my_hp54542(
+      host="", portmap_proxy_host="127.0.0.1", portmap_proxy_port=1111,
+      device="gpib0,7", timeout=4000, device_name="hp54542", raise_on_err=1)
 else:
-	scope=my_hp54542(host="",
-			 device="gpib0,7",  timeout=5000, device_name="hp54542",
-			raise_on_err=1)
-
+  scope = my_hp54542(host="",
+                     device="gpib0,7",  timeout=5000, device_name="hp54542",
+                     raise_on_err=1)
